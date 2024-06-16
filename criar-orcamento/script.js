4679577f7d452457943939af544dab4c9242daf8
Kennedy Pinheiro <kpx@aluno.ifnmg.edu.br>
2024-06-16 11:21:31 -0300

    Add files via upload

--- criar-orcamento/script.js
+++ criar-orcamento/script.js
@@ -1,2 +1,10 @@
+function mostrarCamposProduto() {
+    const cliente = document.getElementById('cliente').value;
+    if (cliente) {
+        document.getElementById('produtoCampos').style.display = 'block';
+    } else {
+        alert('Por favor, selecione um cliente.');
+    }
+}
 
 
