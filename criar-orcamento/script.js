05b072e3f9857a6d91a12ca93a654ba34e0cc25a
xDalete <xdalete@proton.me>
2024-06-17 21:53:12 -0300

    Correções SCSS e JS + Adicionar cliente

--- criar-orcamento/script.js
+++ criar-orcamento/script.js
@@ -1,10 +1,26 @@
-function mostrarCamposProduto() {
-    const cliente = document.getElementById('cliente').value;
-    if (cliente) {
-        document.getElementById('produtoCampos').style.display = 'block';
-    } else {
-        alert('Por favor, selecione um cliente.');
-    }
+import Produto from "../lib/Produto.js";
+// import Cliente from "../lib/Cliente.js";
+import Orcamento from "../lib/Orcamento.js";
+import DB from "../lib/Database.js";
+
+// essa pagina ainda não funciona corretamente
+// cadastre um cliente pela pagina de adicionar cliente e ele vai aparecer no select de cliente 
+
+DB.produto.add(new Produto(-1, "sim", 10, 15, 10))
+DB.cliente.get()[0] && DB.orcamento.add(new Orcamento(-1, DB.cliente.get()[0], DB.produto.get()))
+console.log(DB.produto.get())
+console.log(DB.cliente.get())
+console.log(DB.orcamento.get())
+
+
+
+function updateProdutos() {
+    document.getElementById("cliente").innerHTML = '<option value="">Selecione um cliente</option>'
+    DB.cliente.get().forEach(cliente => {
+        const option = document.createElement('option')
+        option.value = cliente.id
+        option.innerText = cliente.nome
+        document.getElementById("cliente").appendChild(option)
+    })
 }
-
-
+updateProdutos()
--- login/script.js
+++ login/script.js
@@ -1,26 +1,26 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-            let senhaInput = document.getElementById("sen");
-            senhaInput.addEventListener('keydown', function(event) {
-                if (event.key === "Enter") {
-                    salvar();
-                }
-            });
-        });
-
-        function salvar() {
-            let usuario = document.getElementById("log").value.toLowerCase();
-            let senha = document.getElementById("sen").value;
-
-            let usuario1 = "kennedy";
-            let senha1 = "1107";
-
-            let usuario2 = "daniel";
-            let senha2 = "1234";
-
-            if ((usuario === usuario1 && senha === senha1) || (usuario === usuario2 && senha === senha2)) {
-                window.location.href = "../paginainicial/index.html";
-            } else {
-                alert("Usuário não identificado");
-            }
+document.addEventListener('DOMContentLoaded', function () {
+    let senhaInput = document.getElementById("sen");
+    senhaInput.addEventListener('keydown', function (event) {
+        if (event.key === "Enter") {
+            salvar();
         }
+    });
+});
+
+function salvar() {
+    let usuario = document.getElementById("log").value.toLowerCase();
+    let senha = document.getElementById("sen").value;
+
+    let usuario1 = "kennedy";
+    let senha1 = "1107";
+
+    let usuario2 = "daniel";
+    let senha2 = "1234";
+
+    if ((usuario === usuario1 && senha === senha1) || (usuario === usuario2 && senha === senha2)) {
+        window.location.href = "./../inicio/";
+    } else {
+        alert("Usuário não identificado");
+    }
+}
